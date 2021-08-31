722698aff0365ed247301765833ae249b8f276b8
DaniilShaposhnikow <87771174+DaniilShaposhnikow@users.noreply.github.com>
2021-08-31 20:15:09 +0200

    Android-Study-Design
    Android-Study-Design

--- app/src/main/java/com/hfad/testo/PizzaFragment.java
+++ app/src/main/java/com/hfad/testo/PizzaFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.ListFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,14 +19,32 @@ import android.widget.ArrayAdapter;
 /**
  * A fragment representing a list of Items.
  */
-public class PizzaFragment extends ListFragment {
+public class PizzaFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.pizzas));
-        setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+                             Bundle savedInstanceState)
+    {
+//        ArrayAdapter<String> adapter = new ArrayAdapter<>(inflater.getContext(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.pizzas));
+//        setListAdapter(adapter);
+//        return super.onCreateView(inflater, container, savedInstanceState);
+
+        RecyclerView pizzaRecycler = (RecyclerView) inflater.inflate(R.layout.pizza_fragment, container, false);
+        String[] pizzaNames = new String[Pizza.pizzas.length];
+        for (int i = 0; i < pizzaNames.length; i++) {
+            pizzaNames[i] = Pizza.pizzas[i].getName();
+        }
+        int[] pizzaImages = new int[Pizza.pizzas.length];
+        for (int i = 0; i < pizzaImages.length; i++) {
+            pizzaImages[i] = Pizza.pizzas[i].getImageResourceId();
+        }
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(pizzaNames, pizzaImages);
+        pizzaRecycler.setAdapter(adapter);
+//        GridLayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        StaggeredGridLayoutManager layoutManager =           new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        pizzaRecycler.setLayoutManager(layoutManager);
+        return pizzaRecycler;
     }
 }
